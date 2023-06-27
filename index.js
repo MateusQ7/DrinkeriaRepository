c6721222b3c3bdd8edffd1cc5f9867705290edac
mateusquixada <mateusquixada@gmail.com>
2023-06-27 18:57:07 -0300

    Adicionando: Estilo Final

---
+++ index.js
@@ -0,0 +1,18 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+    setInterval( function(){
+        nextImage();
+        console.log("Mudei");
+    }, 4000)
+
+    function nextImage(){
+    count++;
+    if(count > 3){
+    count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
+
+
