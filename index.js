5652c9deb336b2fa071fa16e2a63c8eb5a461970
mateusquixada <mateusquixada@gmail.com>
2023-06-28 14:09:54 -0300

    Adcionando: DOM e começo do nodemailer

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+//Slide animation
 let count = 1;
 document.getElementById("radio1").checked = true;
 
     setInterval( function(){
         nextImage();
-        console.log("Mudei");
     }, 4000)
 
     function nextImage(){
@@ -15,4 +15,42 @@ document.getElementById("radio1").checked = true;
     document.getElementById("radio" + count).checked = true;
 }
 
+function updateSelect1(){
+    let select = document.querySelector("#select-option1");
+    let optionValue = select.options[select.selectedIndex];
 
+    let value = optionValue.value;
+    let text = optionValue.text;
+
+    console.log("Selecionado1:" + text);
+}
+
+function updateSelect2(){
+    let select = document.querySelector("#select-option2");
+    let optionValue = select.options[select.selectedIndex];
+
+    let value = optionValue.value;
+    let text = optionValue.text;
+
+    console.log("Selecionado2:" + text);
+}
+
+function updateSelect3(){
+    let select = document.querySelector("#select-option3");
+    let optionValue = select.options[select.selectedIndex];
+
+    let value = optionValue.value;
+    let text = optionValue.text;
+
+    console.log("Selecionado3:" + text);
+}
+
+function getText(){
+    let text = document.getElementById("text-box").value;
+
+    console.log(text)
+}
+updateSelect1();
+updateSelect2();
+updateSelect3();
+getText();
